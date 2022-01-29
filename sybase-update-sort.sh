79a323158041a8cb94c657ed3ab3e2d051917e7d
Ingleiv Johansen <ingleivj@gmail.com>
2022-01-29 16:48:13 +0100

    Update sybase-update-sort.sh

--- sybase-update-sort.sh
+++ sybase-update-sort.sh
@@ -7,8 +7,8 @@ source /opt/sybase/SYBASE.sh
 # Start SYBASE
 nohup /opt/sybase/ASE-16_0/install/RUN_MYSYBASE &
 
-# Wait just a bit for Sybase to boot
-sleep 10
+# Wait a bit for Sybase to boot
+sleep 30
 /opt/sybase/ASE-16_0/bin/sqllocres -r /opt/tmp/sqlloc.rs
 
 # exit ${RET}
